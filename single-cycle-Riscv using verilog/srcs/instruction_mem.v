7d1850d45b3f5c265709813b0ab525c0421d7b81
Shanmukh_Varma <103655775+Shanmukh20BQ1A0435@users.noreply.github.com>
2026-07-17 17:11:30 +0530

    Update instruction_mem.v

--- single-cycle-Riscv using verilog/srcs/instruction_mem.v
+++ single-cycle-Riscv using verilog/srcs/instruction_mem.v
@@ -18,6 +18,10 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+/*Program Storage: It acts as the "memory" for your CPU, holding 256 instructions (1KB total) that the processor needs to execute.
+Automatic Loading: It automatically pulls your machine code from a file named program.txtthe moment the simulation starts.
+Address Conversion: It converts the CPU's memory address into a line number by dividing by 4 (using the >> 2shift), which helps it find the right instruction quickly.
+Instant Access: It provides the instruction instantly to the processor without waiting for a clock cycle, making the "fetch" stage very fast.*/
 module instruction_mem (
     input clk,
     input rst,
@@ -26,6 +30,9 @@ module instruction_mem (
 );
 
     reg [31:0] mem [0:255];
+    initial begin
+        $readmemh("program.txt", mem);
+    end
 
     assign instruction_out = mem[read_address >> 2];
 
