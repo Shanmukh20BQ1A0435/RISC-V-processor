7510f5ce67420b635d1b3e0b6426fbf790406886
Shanmukh_Varma <103655775+Shanmukh20BQ1A0435@users.noreply.github.com>
2026-07-17 17:44:48 +0530

    Update instruction_mem.v

--- single-cycle-Riscv using verilog/srcs/instruction_mem.v
+++ single-cycle-Riscv using verilog/srcs/instruction_mem.v
@@ -18,9 +18,10 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-/*Program Storage: It acts as the "memory" for your CPU, holding 256 instructions (1KB total) that the processor needs to execute.
-Automatic Loading: It automatically pulls your machine code from a file named program.txtthe moment the simulation starts.
-Address Conversion: It converts the CPU's memory address into a line number by dividing by 4 (using the >> 2shift), which helps it find the right instruction quickly.
+/*Program Storage: It acts as the memory for your CPU, holding 256 instructions that the processor needs to execute.
+Automatic Loading (because of pre defined functtion) It automatically pulls your machine code from a file named program.txtthe moment the simulation starts.
+(Address Conversion)It converts the CPU's memory address into a line number by dividing by 4 (using the >> 2shift), which helps it find the right instruction quickly(for rtl purpose we did this not in real risc it wont exits it follow 
+the byte address format only)
 Instant Access: It provides the instruction instantly to the processor without waiting for a clock cycle, making the "fetch" stage very fast.*/
 module instruction_mem (
     input clk,
@@ -29,9 +30,9 @@ module instruction_mem (
     output [31:0] instruction_out
 );
 
-    reg [31:0] mem [0:255];
+    reg [31:0] mem [0:255];//1 kb
     initial begin
-        $readmemh("program.txt", mem);
+        $readmemh("program.txt", mem);// in built function loads the hex file from program.txt file to mem as soon as initial hits
     end
 
     assign instruction_out = mem[read_address >> 2];
